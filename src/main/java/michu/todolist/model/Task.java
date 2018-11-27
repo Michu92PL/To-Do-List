f4857503a646b34e8c1192d5398e14afaab62127
Michu <mich1992@wp.pl>
2018-11-27 18:21:24 +0100

    Created Task model class

---
+++ src/main/java/michu/todolist/model/Task.java
@@ -0,0 +1,70 @@
+package michu.todolist.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Temporal(TemporalType.DATE)
+    private Date creationDate;
+    @Temporal(TemporalType.DATE)
+    private Date untilWhen;
+    private String description;
+    private short importance;
+    private boolean isActive;
+
+    public Task() {
+    }
+
+    public Task(Date creationDate, Date untilWhen, String description, short importance, boolean isActive) {
+        this.creationDate = creationDate;
+        this.untilWhen = untilWhen;
+        this.description = description;
+        this.importance = importance;
+        this.isActive = isActive;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getUntilWhen() {
+        return untilWhen;
+    }
+
+    public void setUntilWhen(Date untilWhen) {
+        this.untilWhen = untilWhen;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public short getImportance() {
+        return importance;
+    }
+
+    public void setImportance(short importance) {
+        this.importance = importance;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+}
