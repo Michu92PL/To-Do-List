caad1f4ccff0cd78e98a730efae5d67ee8463cd3
Michu <mich1992@wp.pl>
2018-11-27 19:08:45 +0100

    Added TaskServiceImpl class

--- src/main/java/michu/todolist/service/TaskService.java
+++ src/main/java/michu/todolist/service/TaskService.java
@@ -3,6 +3,7 @@ package michu.todolist.service;
 import michu.todolist.model.Task;
 
 import java.util.Collection;
+import java.util.Date;
 
 public interface TaskService {
 
@@ -10,6 +11,6 @@ public interface TaskService {
     void archiveTask(Task task);
     Collection<Task> findAllActive();
     Collection<Task> findAllArchived();
-    Collection<Task> findAllByDate();
+    Collection<Task> findAllByDate(Date date);
     Collection<Task> findAllByImportance(short imp);
 }
---
+++ src/main/java/michu/todolist/service/TaskServiceImpl.java
@@ -0,0 +1,54 @@
+package michu.todolist.service;
+
+import michu.todolist.model.Task;
+import michu.todolist.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private TaskRepository taskRepository;
+
+    @Autowired
+    public TaskServiceImpl(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    public void addTask(Task task) {
+        taskRepository.save(task);
+    }
+
+    @Override
+    public void archiveTask(Task task) {
+        task.setActive(false);
+
+    }
+
+    @Override
+    public Collection<Task> findAllActive() {
+        return taskRepository.findAll().stream().filter(t -> t.isActive()).collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<Task> findAllArchived() {
+        return taskRepository.findAll().stream().filter(t -> !t.isActive()).collect(Collectors.toList());
+
+    }
+
+
+    @Override
+    public Collection<Task> findAllByDate(Date date) {
+        return null;
+    }
+
+    @Override
+    public Collection<Task> findAllByImportance(short imp) {
+        return null;
+    }
+}
