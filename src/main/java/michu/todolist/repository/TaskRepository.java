15b284390508dc39a6170d8f4ba7eb45fc9c566b
Michu <mich1992@wp.pl>
2018-11-27 18:40:59 +0100

    Changed TaskRepository

--- src/main/java/michu/todolist/repository/TaskRepository.java
+++ src/main/java/michu/todolist/repository/TaskRepository.java
@@ -4,8 +4,10 @@ import michu.todolist.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
+    Collection<Task> findAllActive();
 }
