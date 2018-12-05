2ba6ec40033ca9c76568f69379ecea7f998708f9
Michu <mich1992@wp.pl>
2018-12-05 20:24:38 +0100

    Added 3 method declarations

--- src/main/java/michu/todolist/service/TaskService.java
+++ src/main/java/michu/todolist/service/TaskService.java
@@ -7,7 +7,11 @@ import java.util.Date;
 
 public interface TaskService {
 
+
+    void saveChangedTask(Task task);
+    void changeStatus(Long id, boolean status);
     void addTask(Task task);
+    void archiveTask(Long id);
     void archiveTask(Task task);
     Collection<Task> findAllActive();
     Collection<Task> findAllArchived();
